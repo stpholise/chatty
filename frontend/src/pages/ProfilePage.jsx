c60ae8d73d475532d6cf08f4ff08959434fa27c5
stephen olise <stpholise@gmail.com>
2026-02-14 10:22:58 +0100

    settins page with theme toggole from daisyui, and useChart store for messages

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -20,8 +20,8 @@ const ProfilePage = () => {
     };
   };
   return (
-    <div className="h-screen pt-20">
-      <div className="max-w-2xl mx-auto p-4 py-8">
+    <div className="min-h-screen pt-20">
+      <div className="max-w-2xl mx-auto p-4 py-8 ">
         <div className="text-center">
           <h1 className="text-2xl font-semibold">Profile</h1>
           <p className="mt-2">Your prfile information</p>
---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,38 @@
+import { create } from 'zustand'
+import toast from 'react-hot-toast'
+import { axiosInstance } from '../lib/axios'
+
+
+export const useChartStore = create((set) => ({
+    messages: [],
+    users: [],
+    selectedUser: null, 
+    isUsersLoading: false,
+    isMessagesLoading: false,
+    
+    getUsers : async () => {
+        set({ isUserLoading: true})
+        try{
+            const res= await axiosInstance.get("/message/users")
+            set({users: res.data});
+
+        } catch(error) {
+            toast.error(error.response?.data?.messages);
+        }finally {
+            set({isUserLoading: false})
+        }
+    },
+
+
+    getMessages: async (userId) => {
+        set({ isMessagesLoading: true})
+        try{
+            const res = await axiosInstance.get(`/messages/${userId}`)
+            set({messages: res.data})
+        }catch (error) {
+            toast.error(error.response?.data?.messages)
+        }finally {
+            set({ isMessagesLoading: false})
+        }
+    }
+}))
